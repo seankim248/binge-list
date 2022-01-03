752e7ae7344d9d23a5aa29b3d823047b52d1154f
Sean Kim <seankim2480@gmail.com>
2022-01-03 21:24:57 +0000

    made sure that the data response is correct

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'http://img.omdbapi.com/?apikey=[3ba74abf]&t=Lego');
-xhr.responseType = 'json';
-xhr.addEventListener('load', function () {
-  // console.log(xhr.status);
-  // console.log(xhr.response);
-});
+var $cardList = document.querySelector('.card-list');
 
-xhr.send();
+function renderPage1Movies() {
+  var xhr = new XMLHttpRequest();
+  var page1Url = 'https://api.themoviedb.org/3/movie/popular?api_key=ae82140c9c251d2fcd2c3ce9711b3299&language=en-US&page=1';
+  xhr.open('GET', page1Url);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    var movies = xhr.response.results;
+    for (var i = 0; i < movies.length; i++) {
+      var poster = document.createElement('img');
+      poster.setAttribute('src', 'https://image.tmdb.org/t/p/original' + movies[i].poster_path);
+      $cardList.appendChild(poster);
+    }
+  });
+
+  xhr.send();
+}
+
+renderPage1Movies();
